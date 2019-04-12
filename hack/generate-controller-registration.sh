c86dd00b38309ffbc1f1b48367337ed9ceda0ab5
Tim Usner <tim.usner@sap.com>
2019-04-12 08:52:15 +0200

    Improve generate script and local dev experience

--- hack/generate-controller-registration.sh
+++ hack/generate-controller-registration.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 set -e
+set -o pipefail
 
 DIRNAME="$(echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
 source "$DIRNAME/common.sh"
