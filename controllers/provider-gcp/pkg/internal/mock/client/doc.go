d43b33a86bc7e4dc7695adee1e99ab2680c50b7b
Sebastian Stauch <sebastian.stauch@sap.com>
2019-04-15 08:37:35 +0200

    delete remnant route entries

--- controllers/provider-gcp/pkg/internal/mock/client/doc.go
+++ controllers/provider-gcp/pkg/internal/mock/client/doc.go
@@ -1,3 +1,3 @@
-//go:generate mockgen -package=client -destination=mocks.go github.com/gardener/gardener-extensions/controllers/provider-gcp/pkg/internal/client Interface,FirewallsService,FirewallsListCall,FirewallsDeleteCall
+//go:generate mockgen -package=client -destination=mocks.go github.com/gardener/gardener-extensions/controllers/provider-gcp/pkg/internal/client Interface,FirewallsService,RoutesService,FirewallsListCall,RoutesListCall,FirewallsDeleteCall,RoutesDeleteCall
 
 package client
