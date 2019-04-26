ce508e8d9487dfad99c5dadb1dae30d81b45dabe
Rafael Franzke <rafael.franzke@sap.com>
2019-04-26 17:06:35 +0200

    Fix clean script

--- hack/clean.sh
+++ hack/clean.sh
@@ -24,5 +24,5 @@ header_text "Clean"
 for source_tree in ${SOURCE_TREES[@]}; do
   find "$(dirname "$source_tree")" -type f -name "controller-registration.yaml" -exec rm '{}' \;
   find "$(dirname "$source_tree")" -type f -name "zz_*.go" -exec rm '{}' \;
-  grep -lr --include="*.go" "//go:generate packr2" | xargs -i packr2 clean "{}/.."
+  grep -lr --include="*.go" "//go:generate packr2" . | xargs -I {} packr2 clean "{}/.."
 done
