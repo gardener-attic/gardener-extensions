e06352d0f9717b14601176bffac61c8cf1b1e94c
Rafael Franzke <rafael.franzke@sap.com>
2019-08-21 15:55:16 +0200

    Implement `ProviderProfile` for Packet
    * We will move the machine image mapping from the componentconfig to the ProviderProfile soon.
    * Implemented validation for `ProviderProfileConfig` (copied from https://github.com/gardener/gardener/blob/master/pkg/apis/garden/validation/validation.go and https://github.com/gardener/gardener/blob/master/pkg/apis/garden/validation/validation_test.go).

---
+++ controllers/provider-packet/pkg/apis/packet/validation/validation_providerprofile.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package validation
+
+import (
+	"fmt"
+
+	apispacket "github.com/gardener/gardener-extensions/controllers/provider-packet/pkg/apis/packet"
+
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+// ValidateProviderProfileConfig validates a ProviderProfileConfig object.
+func ValidateProviderProfileConfig(providerProfile *apispacket.ProviderProfileConfig) field.ErrorList {
+	allErrs := field.ErrorList{}
+
+	machineImagesPath := field.NewPath("machineImages")
+	if len(providerProfile.MachineImages) == 0 {
+		allErrs = append(allErrs, field.Required(machineImagesPath, "must provide at least one machine image"))
+	}
+	for i, machineImage := range providerProfile.MachineImages {
+		idxPath := machineImagesPath.Index(i)
+
+		if len(machineImage.Name) == 0 {
+			allErrs = append(allErrs, field.Required(idxPath.Child("name"), "must provide a name"))
+		}
+
+		if len(machineImage.Versions) == 0 {
+			allErrs = append(allErrs, field.Required(idxPath.Child("versions"), fmt.Sprintf("must provide at least one version for machine image %q", machineImage.Name)))
+		}
+		for j, version := range machineImage.Versions {
+			jdxPath := idxPath.Child("versions").Index(j)
+
+			if len(version.Version) == 0 {
+				allErrs = append(allErrs, field.Required(jdxPath.Child("version"), "must provide a version"))
+			}
+			if len(version.ID) == 0 {
+				allErrs = append(allErrs, field.Required(jdxPath.Child("id"), "must provide an id"))
+			}
+		}
+	}
+
+	return allErrs
+}
