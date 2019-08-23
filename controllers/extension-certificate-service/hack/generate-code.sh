35f5c7fe505199152796921ae14d7292276994c7
ialidzhikov <i.alidjikov@gmail.com>
2019-08-23 19:58:10 +0300

    Fix ./.ci/prepare_release to generate the appropriate chart
    Signed-off-by: ialidzhikov <i.alidjikov@gmail.com>

--- controllers/extension-certificate-service/hack/generate-code.sh
+++ controllers/extension-certificate-service/hack/generate-code.sh
@@ -14,25 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-function headers() {
-  echo '''/*
-Copyright (c) YEAR SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-'''
-}
-
 function usage() {
   cat <<EOM
   Usage: $(basename $0) <output-package> <internal-apis-package> <extensiona-apis-package> <groups-versions>
@@ -63,7 +44,7 @@ bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh 
   $EXT_APIS_PKG \
   $EXT_APIS_PKG \
   $GROUPS_WITH_VERSIONS \
-  -h <(headers)
+  --go-header-file "${PROJECT_ROOT}/hack/LICENSE_BOILERPLATE.txt"
 
 bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
   conversion \
@@ -71,4 +52,4 @@ bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh 
   $EXT_APIS_PKG \
   $EXT_APIS_PKG \
   $GROUPS_WITH_VERSIONS \
-  -h <(headers)
+  --go-header-file "${PROJECT_ROOT}/hack/LICENSE_BOILERPLATE.txt"
