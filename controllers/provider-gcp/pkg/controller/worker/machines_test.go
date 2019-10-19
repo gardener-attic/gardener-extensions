0e8545f88a90572124d9a6c7a023070092cff2e8
Zaalouk, Adel <adel.zalok.89@gmail.com>
2019-10-19 15:03:13 +0200

    Enable cloud-nat for GCP

--- controllers/provider-gcp/pkg/controller/worker/machines_test.go
+++ controllers/provider-gcp/pkg/controller/worker/machines_test.go
@@ -262,6 +262,7 @@ var _ = Describe("Machines", func() {
 				var (
 					defaultMachineClass = map[string]interface{}{
 						"region":             region,
+						"disableExternalIP":  true,
 						"canIpForward":       true,
 						"deletionProtection": false,
 						"description":        fmt.Sprintf("Machine of Shoot %s created by machine-controller-manager.", name),
