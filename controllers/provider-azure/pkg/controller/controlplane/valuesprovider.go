aae3ee9339bec8ecf36f8e1fa56ed79bd15bbdac
Vladimir Nachev <vladimir.penkov.nachev@gmail.com>
2020-01-09 20:37:15 +0200

    Azure-CCM: Bundle cloudProviderRateLimitQPS[Write] to the maxNodes
    Ref: https://github.com/gardener/gardener-extensions/pull/532
    
    ```improvement operator
    An issue where cloud controller manager was self-rate-limited for azure shoot clusters with more than 10 nodes has been fixed.
    ```

--- controllers/provider-azure/pkg/controller/controlplane/valuesprovider.go
+++ controllers/provider-azure/pkg/controller/controlplane/valuesprovider.go
@@ -213,6 +213,11 @@ func getConfigChartValues(
 		return nil, errors.Wrapf(err, "could not determine subnet, availability set, route table or security group name from infrastructureStatus of controlplane '%s'", util.ObjectName(cp))
 	}
 
+	var maxNodes int32
+	for _, worker := range cluster.Shoot.Spec.Provider.Workers {
+		maxNodes = maxNodes + worker.Maximum
+	}
+
 	// Collect config chart values.
 	values := map[string]interface{}{
 		"kubernetesVersion": cluster.Shoot.Spec.Kubernetes.Version,
@@ -226,6 +231,7 @@ func getConfigChartValues(
 		"routeTableName":    routeTableName,
 		"securityGroupName": securityGroupName,
 		"region":            cp.Spec.Region,
+		"maxNodes":          maxNodes,
 	}
 
 	if infraStatus.Networks.VNet.ResourceGroup != nil {
